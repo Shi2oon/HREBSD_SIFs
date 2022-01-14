ba36c97ba91d12d06d68a69ffe3702235739a701
Abdo Koko <45297426+Shi2oon@users.noreply.github.com>
2022-01-14 02:22:02 +0000

    Update Example_input_desk.m

--- Example_input_desk.m
+++ Example_input_desk.m
@@ -8,7 +8,7 @@ Prop.units.St = 'Pa';
 Prop.units.xy = 'm';
 Prop.stressstat = 'plane_stress';
 
-[J,KI,KII,KIII] = KIII_2D(alldata,Prop); % as desigignated maps
+[J,KI,KII,KIII] = KIII_2D_v2(alldata,Prop); % as desigignated maps
 [J,KI,KII,KIII] = KIII_2D(MatProp);         % or just MatProp
 
 %% HR-EBSD data
