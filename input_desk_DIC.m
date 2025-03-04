5280cf2d88cb657cfec41143b745ac554e264696
Abdo Koko <45297426+Shi2oon@users.noreply.github.com>
2025-03-04 11:48:18 +0000

    better

--- input_desk_DIC.m
+++ input_desk_DIC.m
@@ -1,6 +1,8 @@
+%% input desk for 3D istropic materials
 clc;clear;close all
 % Make sure the grid spacing is equal in x and y direction, also make sure
 % the crack is at the centre of the map
+addpath(genpath([pwd '\functions']));
 Prop.E = 210e9;  % Young's Modulus, you can also input a stifness matrix
 Prop.nu = 0.3;     % Poisson ratio         
 Prop.units.St = 'Pa';  
@@ -8,7 +10,46 @@ Prop.units.xy = 'mm';% meter (m) or milmeter (mm) or micrometer(um);
 Prop.stressstat = 'plane_stress';% 'plane_stress' OR 'plane_strain'
 Prop.Operation = 'DIC'; % 'DIC' for raw 2D and 3D DIC data
 DataDirect = fullfile(pwd,'Data','1KI-2KII-3KII_Data.dat'); % file locatio
-Prop.results = fullfile(pwd,'Data','1KI-2KII-3KII_Data');
+% Prop.SavingD = fullfile(pwd,'Data','1KI-2KII-3KII_Data');
+
+% end of user input
+Data = importdata(DataDirect);
+[K,KI,KII,KIII,J,M,Maps] = M_J_KIII_2D(Data.data,Prop);
+
+%% for Ramberg-Osgood tortuous crack
+clc;clear;close all
+% Make sure the grid spacing is equal in x and y direction, also make sure
+% the crack is at the centre of the map
+addpath(genpath([pwd '\functions']));
+Prop.E = 210e9;  % Young's Modulus, you can also input a stifness matrix
+Prop.nu = 0.3;     % Poisson ratio     
+Prop.yield = 4E9; % Yield stress in Pa
+Prop.Yield_offset = 1.24; % Hardening coefficient
+Prop.Exponent = 26.67; % Hardening exponent
+Prop.units.St = 'Pa';  
+Prop.units.xy = 'm';% meter (m) or milmeter (mm) or micrometer(um);
+Prop.stressstat = 'plane_stress';% 'plane_stress' OR 'plane_strain'
+Prop.Operation = 'DIC'; % 'DIC' for raw 2D and 3D DIC data
+DataDirect = fullfile(pwd,'Data','3660Jm.dat'); % file locatio
+% Prop.SavingD = fullfile(pwd,'Data','1KI-2KII-3KII_Data');
+
+% end of user input
+Data = importdata(DataDirect);
+[K,KI,KII,KIII,J,M,Maps] = M_J_KIII_2D(Data.data,Prop);
+
+%% %% input desk for 2D istropic materials
+clc;clear;close all
+% Make sure the grid spacing is equal in x and y direction, also make sure
+% the crack is at the centre of the map
+addpath(genpath([pwd '\functions']));
+Prop.E = 70e9;  % Young's Modulus, you can also input a stifness matrix
+Prop.nu = 0.321;     % Poisson ratio         
+Prop.units.St = 'Pa';  
+Prop.units.xy = 'nm';% meter (m) or milmeter (mm) or micrometer(um);
+Prop.stressstat = 'plane_stress';% 'plane_stress' OR 'plane_strain'
+Prop.Operation = 'DIC'; % 'DIC' for raw 2D and 3D DIC data
+DataDirect = fullfile(pwd,'Data','Tortuous_Crack_Data.dat'); % file locatio
+% Prop.SavingD = fullfile(pwd,'Data','1KI-2KII-3KII_Data');
 
 % end of user input
 Data = importdata(DataDirect);
